a62d7786ae8daca2bd4dcb94e6bf57fec37d49a7
github-action-benchmark <github@users.noreply.github.com>
2026-02-27 01:52:10 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 1d1dfe5d13f1f93f0a00b70bebf9626a52ce838a

---
+++ dev/bench/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1772157130882,
+  "repoUrl": "https://github.com/Jeromearsene/levita",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "9333686+Jeromearsene@users.noreply.github.com",
+            "name": "Jerome",
+            "username": "Jeromearsene"
+          },
+          "committer": {
+            "email": "9333686+Jeromearsene@users.noreply.github.com",
+            "name": "Jerome",
+            "username": "Jeromearsene"
+          },
+          "distinct": true,
+          "id": "1d1dfe5d13f1f93f0a00b70bebf9626a52ce838a",
+          "message": "chore: configure trusted publishing for NPM",
+          "timestamp": "2026-02-27T02:24:48+01:00",
+          "tree_id": "b9232d311ef6621fc72b1a239fc3c9218a57178c",
+          "url": "https://github.com/Jeromearsene/levita/commit/1d1dfe5d13f1f93f0a00b70bebf9626a52ce838a"
+        },
+        "date": 1772157130443,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Levita — basic init",
+            "value": 11614.45,
+            "unit": "ops/sec"
+          },
+          {
+            "name": "Levita — init with glare + shadow",
+            "value": 6879.2,
+            "unit": "ops/sec"
+          },
+          {
+            "name": "Levita — init with 5 layers",
+            "value": 4589.9,
+            "unit": "ops/sec"
+          },
+          {
+            "name": "Levita — pointer move update",
+            "value": 10656.89,
+            "unit": "ops/sec"
+          },
+          {
+            "name": "Levita — pointer move with glare + shadow",
+            "value": 6523.17,
+            "unit": "ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
